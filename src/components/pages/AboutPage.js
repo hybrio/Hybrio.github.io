0d6b7db72ec1b0eee07954fed452043a9a1ad705
Alexander M <alexander.maehl@gmail.com>
2020-10-10 02:23:54 +1300

    added about me

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -4,7 +4,18 @@ class AboutPage extends React.Component {
   render() {
     return (
       <div>
-        &nbsp;page under construction
+        <h1>About me.</h1>
+          <div id="about">
+            <p id="about-text">
+              Hi i'm Alexander
+              <br/><br/><br/>
+              I am a software developer from Auckland, New Zealand who enjoys writing code and solving problems.
+              <br/><br/>
+              I will soon be (covid permitting) a 2020 Computer Systems Engineering (Hons) Graduate from the University of Auckland.
+              <br/><br/>
+              Unfortunately I don’t have anything too exciting to show you as of yet (I’m still retrieving my older uni projects) but watch this space.
+          </p>
+        </div>
       </div>
     );
   }
